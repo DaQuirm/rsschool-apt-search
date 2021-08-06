62047e5838229fbb4edd38cf463955cfba34a506
DaQuirm <leo.daquirm@gmail.com>
2021-08-06 20:13:04 +0200

    Use predicate builders and combinators for filtering

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,30 @@
 module Main where
 
-import Data.Apartment (Apartment(..), Bezirk (Charlottenburg))
+import Data.Apartment (Apartment(..), Bezirk (..))
 import Data.Fixture.Apartments (apartments)
 
-r0 :: [Apartment]
-r0 = filter (\Apartment { rent, bezirk } -> rent < 800 && bezirk == Charlottenburg) apartments
+type Predicate = Apartment -> Bool
+
+(&&&) :: Predicate -> Predicate -> Predicate
+pa &&& pb = \apartment -> pa apartment && pb apartment
+
+rentBelow :: Float -> Predicate
+rentBelow maxRent Apartment { rent } = rent < maxRent
+
+inBezirk :: Bezirk -> Predicate
+inBezirk whichBezirk Apartment { bezirk } = bezirk == whichBezirk
+
+areaAtLeast :: Float -> Predicate
+areaAtLeast minArea Apartment { area } = area >= minArea
+
+findApartment :: Predicate -> [Apartment]
+findApartment p = filter p apartments
 
 r1 :: [Bezirk]
-r1 = map (\Apartment { bezirk } -> bezirk) r0
+r1 = map (\Apartment { bezirk } -> bezirk) (findApartment (rentBelow 700 &&& inBezirk Neukölln &&& areaAtLeast 50))
 
--- >>> r0
--- [Apartment {address = "Otto-Suhr-Allee 114", rooms = 1, rent = 620.0, bezirk = Charlottenburg, area = 50.0, floor = Ground, lift = False},Apartment {address = "Alt Moabit 37", rooms = 2, rent = 389.62, bezirk = Charlottenburg, area = 59.85, floor = Floor 3, lift = False},Apartment {address = "Kurf\252rstendamm 105", rooms = 1, rent = 495.0, bezirk = Charlottenburg, area = 41.0, floor = Floor 3, lift = True}]
+-- >>> (findApartment (rentBelow 700 &&& inBezirk Neukölln &&& areaAtLeast 50))
+-- [Apartment {address = "Marienfelder Allee 27", rooms = 2, rent = 600.0, bezirk = Neukölln, area = 52.5, floor = Floor 4, lift = True}]
 
 main :: IO ()
 main = mempty
