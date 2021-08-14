b4b927b4b9b68356756ae35d4e90c525bd895943
DaQuirm <leo.daquirm@gmail.com>
2021-08-14 17:54:36 +0200

    Add pretty printing for hls comment eval

--- app/Main.hs
+++ app/Main.hs
@@ -38,8 +38,40 @@ r1 = findApartment
   &&& (rooms `atLeast` 2)
   )
 
--- >>> r1
--- [Apartment {address = "Marienfelder Allee 27", rooms = 2, rent = 600.0, bezirk = Neukölln, area = 52.5, floor = Floor 4, lift = True},Apartment {address = "Alt Moabit 37", rooms = 2, rent = 389.62, bezirk = Charlottenburg, area = 59.85, floor = Floor 3, lift = False},Apartment {address = "M\252llerstr. 29", rooms = 2, rent = 628.0, bezirk = PrenzlauerBerg, area = 56.13, floor = Ground, lift = False}]
+{-
+>>> import qualified Data.Text.Lazy as TL
+>>> import Text.Pretty.Simple
+>>> prettyPrint v = error (TL.unpack $ pShowNoColor v) :: IO String
+>>> prettyPrint r1
+[ Apartment
+    { address = "Marienfelder Allee 27"
+    , rooms = 2
+    , rent = 600.0
+    , bezirk = Neukölln
+    , area = 52.5
+    , floor = Floor 4
+    , lift = True
+    }
+, Apartment
+    { address = "Alt Moabit 37"
+    , rooms = 2
+    , rent = 389.62
+    , bezirk = Charlottenburg
+    , area = 59.85
+    , floor = Floor 3
+    , lift = False
+    }
+, Apartment
+    { address = "Müllerstr. 29"
+    , rooms = 2
+    , rent = 628.0
+    , bezirk = PrenzlauerBerg
+    , area = 56.13
+    , floor = Ground
+    , lift = False
+    }
+]
+-}
 
 main :: IO ()
 main = mempty
